1e11ca37c7304452703934d676fc8289353cd45c
Sergey <sergey13.09.1998@gmail.com>
2025-08-30 13:53:08 +0300

    add task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,24 +11,26 @@ function transformStateWithClones(state, actions) {
   let prevState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      let stateCopy = { ...prevState };
+    let stateCopy = { ...prevState };
 
-      stateCopy = Object.assign(stateCopy, action.extraData);
-      history.push({ ...stateCopy });
-      prevState = stateCopy;
-    } else if (action.type === 'removeProperties') {
-      const stateCopy = { ...prevState };
-
-      for (const key of action.keysToRemove) {
-        delete stateCopy[key];
-      }
-      prevState = stateCopy;
-      history.push({ ...stateCopy });
-    } else if (action.type === 'clear') {
-      prevState = {};
-      history.push({ ...prevState });
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = Object.assign(stateCopy, action.extraData);
+        prevState = stateCopy;
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        prevState = stateCopy;
+        break;
+      case 'clear':
+        prevState = {};
+        break;
+      default:
+        throw new Error('Unsupported action type: ' + action.type);
     }
+    history.push({ ...prevState });
   }
 
   return history;
